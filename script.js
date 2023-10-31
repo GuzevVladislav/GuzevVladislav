dbcde19b9f30442310362503546c3d09ead1f9bc
GuzevVladislav <93818043+GuzevVladislav@users.noreply.github.com>
2023-10-31 10:28:04 +0300

    6

--- script.js
+++ script.js
@@ -1,24 +1,134 @@
-let goods = document.getElementsByClassName("good");
-let input = document.querySelector('input');
-let res = document.getElementById('result');
-let sum = document.getElementById('sum');
-let good;
-for (let i = 0; i < 3; i++) { // выведет 0, затем 1, затем 2
-    goods[i].onclick = function()
-    {
-        good = goods[i].innerHTML;
+let select = document.getElementById("Goods");
+let amount = document.getElementById("input__amount");
+let price = document.getElementById('main__price');
+let radios = document.getElementById("radios");
+let checkbox = document.getElementById("checkboxes");
+let radio = document.getElementsByClassName("radio");
+let checkboxes = document.getElementsByClassName('check');
+let btn = document.getElementById('kol');
 
-    }
-}
+let prodTypes = [150, 240, 119];
+let prod;
 
-res.onclick = function ()
+for(let i = 0; i < 2; i++)
 {
-    if(input.value < 0) alert("Введено отрицательное число");
-    else 
+    checkboxes[i].onclick = function()
     {
-        if(good == "Хлеб 30 руб.") sum.innerHTML =  input.value * 30;
-        if(good == "Молоко 70 руб.") sum.innerHTML = (input.value * 70);
-        if(good == "Сыр 140 руб.") sum.innerHTML = (input.value * 140);
-    
+        check1();
     }
 }
+
+select.addEventListener("change", function()
+{
+    for(let i = 0; i < radio.length; i++)
+    {
+        radio[i].onclick = function ()
+        {
+            prod += parseInt(radio[i].value);
+            update();
+            prod -= parseInt(radio[i].value);
+        }
+    } 
+    
+
+    if(amount.value > -1)
+    {
+        if(select.value == 1)
+        {
+            checkboxes[0].checked = false;
+            checkboxes[1].checked = false;
+            prod = prodTypes[0];
+            radios.style.display = "none";
+            checkbox.style.display = "none";
+            price.innerHTML = prod * amount.value;
+    
+        }if(select.value == 2)
+        {
+            checkboxes[0].checked = false;
+            checkboxes[1].checked = false;
+            prod = prodTypes[1];
+            price.innerHTML = prod * amount.value;
+            checkbox.style.display = "none";
+
+            radios.style.display ="block";
+    
+        }
+        if(select.value == 3)
+        {
+            prod = prodTypes[2];
+            radios.style.display = "none";
+            checkbox.style.display = "block";
+            price.innerHTML = prod * amount.value;
+
+        }
+    } else 
+    {
+        alert("Введено отрицательное значение");
+    }
+
+
+    
+});
+
+function update ()
+{
+    price.innerHTML = prod * amount.value;
+}
+
+
+
+function check1 ()
+{
+    if(checkboxes[0].checked & !checkboxes[1].checked)
+    {
+        prod += parseInt(1);
+        update();
+        prod -= parseInt(1);
+            
+    }
+        
+    else if(!checkboxes[0].checked & checkboxes[1].checked)
+    {
+        prod += parseInt(2);
+        update();
+        prod -= parseInt(2);
+    }
+    else if(checkboxes[0].checked & checkboxes[1].checked)
+    {
+        prod += parseInt(3);
+        update();
+        prod -= parseInt(3);
+    }
+    else { update();}
+}
+    
+
+
+
+// function getPrices() {
+//     return {
+//       prodTypes: [150, 240, 119],
+//       prodOptions: {
+//         option2: 10,
+//         option3: 5,
+//       },
+//       prodProperties: [1, 2, 3]
+//     };
+// }
+
+
+
+        
+
+
+
+
+    
+
+
+
+
+
+
+
+
