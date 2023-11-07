7fd47d8ddb211f34702d11c8f2301743d7ef7b30
GuzevVladislav <93818043+GuzevVladislav@users.noreply.github.com>
2023-11-07 10:07:49 +0300

    7

--- script.js
+++ script.js
@@ -1,144 +1,81 @@
-let select = document.getElementById("Goods");
-let amount = document.getElementById("input__amount");
-let price = document.getElementById('main__price');
-let radios = document.getElementById("radios");
-let checkbox = document.getElementById("checkboxes");
-let radio = document.getElementsByClassName("radio");
-let checkboxes = document.getElementsByClassName('check');
-let btn = document.getElementById('kol');
+let slider = document.getElementsByClassName("slider-img");
+let prev = document.getElementById("prev");
+let next = document.getElementById('next');
+let pages = document.getElementsByClassName("point");
 
-let prodTypes = [150, 240, 119];
-let prod;
+let imgs = {
+    1: "/1.jpg",
+    2: "/2.jpg",
+    3: "/3.jpg",
+    4: "/4.jpg",
+    5: "/5.jpg",
+    6: "/6.jpg",
+    7: "/7.jpg",
+    8: "/8.jpg"
+};
 
-function amount0()
+let curentimg = [1,2,3];
+
+function showPoints()
 {
-    if(amount.value < -1)
+    let left = curentimg[0];
+    let right = curentimg[2];
+    if(left >= 0 & right <= 3)
     {
-        alert("Введено отрицательное значение");
+        pages[0].style.backgroundColor = "#000000";
+        pages[1].style.backgroundColor = "#fff";
+        pages[2].style.backgroundColor = "#fff";
     }
+    if(left >= 3 & right <= 6)
+    {
+        pages[0].style.backgroundColor = "#fff";
+        pages[1].style.backgroundColor = "#000000";
+        pages[2].style.backgroundColor = "#fff";
+    }
+    if(left >= 6 && right <= 8)
+    {
+        pages[0].style.backgroundColor = "#fff";
+        pages[1].style.backgroundColor = "#fff  ";
+        pages[2].style.backgroundColor = "#000000";
+    }
+
 }
 
-for(let i = 0; i < 2; i++)
+function show()
 {
-    checkboxes[i].onclick = function()
-    {
-        amount0();
-        check1();
-        
-    }
+    slider[0].src = imgs[curentimg[0]];
+    slider[1].src = imgs[curentimg[1]];
+    slider[2].src = imgs[curentimg[2]];
+    showPoints();
 }
 
-select.addEventListener("change", function()
+next.onclick = function()
 {
-    for(let i = 0; i < radio.length; i++)
+    let k = curentimg[2] + 1;
+    if(k > 8)
     {
-        radio[i].onclick = function ()
-        {
-            prod += parseInt(radio[i].value);
-            update();
-            prod -= parseInt(radio[i].value);
-        }
-    } 
-    
-
-    if(amount.value > -1)
-    {
-        if(select.value == 1)
-        {
-            checkboxes[0].checked = false;
-            checkboxes[1].checked = false;
-            prod = prodTypes[0];
-            radios.style.display = "none";
-            checkbox.style.display = "none";
-            price.innerHTML = prod * amount.value;
-    
-        }if(select.value == 2)
-        {
-            checkboxes[0].checked = false;
-            checkboxes[1].checked = false;
-            prod = prodTypes[1];
-            price.innerHTML = prod * amount.value;
-            checkbox.style.display = "none";
-
-            radios.style.display ="block";
-    
-        }
-        if(select.value == 3)
-        {
-            prod = prodTypes[2];
-            radios.style.display = "none";
-            checkbox.style.display = "block";
-            price.innerHTML = prod * amount.value;
-
-        }
-    } else 
-    {
-        alert("Введено отрицательное значение");
+        k %= 8;
     }
 
-
-    
-});
-
-function update ()
-{
-    price.innerHTML = prod * amount.value;
+    curentimg[0] = curentimg[1];
+    curentimg[1] = curentimg[2];
+    curentimg[2] = k;
+    show();
 }
-
-
-
-function check1 ()
+prev.onclick = function()
 {
-    if(checkboxes[0].checked & !checkboxes[1].checked)
+    curentimg[2] = curentimg[1];
+    curentimg[1] = curentimg[0];
+    if(curentimg[0] - 1  == 0)
     {
-        prod += parseInt(1);
-        update();
-        prod -= parseInt(1);
-            
+        curentimg[0] = 8;
     }
-        
-    else if(!checkboxes[0].checked & checkboxes[1].checked)
-    {
-        prod += parseInt(2);
-        update();
-        prod -= parseInt(2);
+    else {
+        curentimg[0]--;
+
     }
-    else if(checkboxes[0].checked & checkboxes[1].checked)
-    {
-        prod += parseInt(3);
-        update();
-        prod -= parseInt(3);
-    }
-    else { update();}
+
+    show();
 }
-    
-
-
-
-// function getPrices() {
-//     return {
-//       prodTypes: [150, 240, 119],
-//       prodOptions: {
-//         option2: 10,
-//         option3: 5,
-//       },
-//       prodProperties: [1, 2, 3]
-//     };
-// }
-
-
-
-        
-
-
-
-
-    
-
-
-
-
-
-
 
 
